e58e9cf135b134739ae3d059bc6ff985d5b875e9
l7aj-405 <mohamed.agday.ss@gmail.com>
2024-02-05 09:26:35 +0100

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Col, Row } from "react-bootstrap";
 import colorSharp from "../assets/img/color-sharp.png"
 import headerImg from "../assets/img/baner-imag1.jpg";
 
@@ -10,12 +11,22 @@ function About() {
                 <div className="col-12">
                     <div className="about-bx wow zoomIn">
                         <h2>About</h2>
-                        <div className='about-content'>
+                        <div >
+                        <Row className="aligh-items-center">
+
+                        <Col xs={12} md={6} xl={5}>
+
                             <div className=''>
                             <img className="about-img" src={headerImg} alt="Header Img" />
 
                             </div>
+                        </Col>
+                        <Col xs={12} md={6} xl={7} >
+
+                            
                             <p>Passionate and skilled Full Stack Developer, specializing in the creation of robust web applications. Possessing a strong expertise in both front-end and back-end development, I have advanced skills in technologies such as React JS, Laravel, and SQL. My academic journey at ISTA NTIC Beni Mellal, coupled with hands-on projects, has enhanced my understanding of digital development concepts.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        </Col>
+                        </Row>
                         </div>
                     </div>
                 </div>
