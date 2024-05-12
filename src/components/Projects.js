ab0540e87de620cd256eead3582a1feb6f45c89a
l7aj-405 <mohamed.agday.ss@gmail.com>
2024-05-12 21:22:13 +0100

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,18 +6,14 @@ import projImg3 from "../assets/img/sidebar.png";
 import projImg4 from "../assets/img/musicapp.png";
 import projImg5 from "../assets/img/admin_space.png";
 import projImg6 from "../assets/img/jamilafood.png";
+import projImg7 from "../assets/img/MENARA1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
-  const projects = [
-    {
-      title: "Website for an Office of studies",
-      description: "Content management,users & project requests",
-      imgUrl: projImg1,
-    },
+  const reactprojects = [
+    
     {
       title: "Hotel Space",
       description: "Design & Development",
@@ -28,11 +24,7 @@ export const Projects = () => {
       description: "API library",
       imgUrl: projImg3,
     },
-    {
-      title: "Management of correspondence",
-      description: "Court App Program",
-      imgUrl: projImg4,
-    },
+    
     {
       title: "Admin Design",
       description: "Design & Development",
@@ -44,6 +36,35 @@ export const Projects = () => {
       imgUrl: projImg6,
     },
   ];
+  const laravelprojects = [
+    
+    
+    
+    {
+      title: "Management of correspondence",
+      description: "Court App Program",
+      imgUrl: projImg4,
+    },
+    
+    
+  ];
+  const complixprojects = [
+    {
+      title: "Website for an Office of studies",
+      description: "Content management,users & project requests",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Website for Menara Holding",
+      description: "Content management,users, Order & Products ",
+      imgUrl: projImg7,
+    },
+    
+    
+    
+    
+   
+  ];
 
   return (
     <section className="project" id="projects">
@@ -51,51 +72,71 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>I take pride in my ability to seamlessly connect the dots between front-end and back-end, creating cohesive applications that not only look good but also perform exceptionally well.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    I take pride in my ability to seamlessly connect the dots
+                    between front-end and back-end, creating cohesive
+                    applications that not only look good but also perform
+                    exceptionally well.
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">React & Design</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Laravel & System</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Complex Projects</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {reactprojects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {laravelprojects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {complixprojects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
